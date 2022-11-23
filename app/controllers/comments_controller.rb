ea2f3eace4b9324437678a4111bfe82ef484b419
Migz_Arre <deathslayermigz@gmail.com>
2022-11-23 08:49:21 +0800

    Adding Delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,12 @@ class CommentsController < ApplicationController
         redirect_to article_path(@article)
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+    end
+
     private
         def comment_params
             params.require(:comment).permit(:commenter, :body, :status)
